0677ac5319a859d4e988c73e4ed30549f0eb5a93
niklas-aac <129318684+niklas-aac@users.noreply.github.com>
2023-04-19 15:09:32 +0200

    Update phy_header.h

--- include/gnuradio/mymodule/phy_header.h
+++ include/gnuradio/mymodule/phy_header.h
@@ -9,7 +9,7 @@
 #ifndef INCLUDED_MYMODULE_PHY_HEADER_H
 #define INCLUDED_MYMODULE_PHY_HEADER_H
 
-#include "/home/inets/Workspace/OOT_modules/gr-mymodule/include/gnuradio/mymodule/api.h"
+#include <phy_header/api.h>
 #include <gnuradio/digital/packet_header_default.h>
 // #include <gnuadio/block.h>
 
